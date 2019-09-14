7e64f7f1e141530a376d7957cd3411090b4fb671
Karol Galanciak <karol.galanciak@gmail.com>
2019-09-14 07:51:08 +0200

    update-payment-intern-for-stripe

--- lib/active_merchant/billing/gateways/stripe_payment_intents.rb
+++ lib/active_merchant/billing/gateways/stripe_payment_intents.rb
@@ -2,7 +2,12 @@ require 'active_support/core_ext/hash/slice'
 
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
+    # This gateway uses the current Stripe {Payment Intents API}[https://stripe.com/docs/api/payment_intents].
+    # For the legacy API, see the Stripe gateway
     class StripePaymentIntentsGateway < StripeGateway
+
+      self.supported_countries = %w(AT AU BE BR CA CH DE DK ES FI FR GB HK IE IT JP LU MX NL NO NZ PT SE SG US)
+
       ALLOWED_METHOD_STATES = %w[automatic manual].freeze
       ALLOWED_CANCELLATION_REASONS = %w[duplicate fraudulent requested_by_customer abandoned].freeze
       CREATE_INTENT_ATTRIBUTES = %i[description statement_descriptor receipt_email save_payment_method]
@@ -22,6 +27,7 @@ module ActiveMerchant #:nodoc:
         add_connected_account(post, options)
         add_shipping_address(post, options)
         setup_future_usage(post, options)
+        add_exemption(post, options)
 
         CREATE_INTENT_ATTRIBUTES.each do |attribute|
           add_whitelisted_attribute(post, options, attribute)
@@ -30,10 +36,6 @@ module ActiveMerchant #:nodoc:
         commit(:post, 'payment_intents', post, options)
       end
 
-      def authorize(money, payment_method, options = {})
-        create_intent(money, payment_method, options)
-      end
-      
       def show_intent(intent_id, options)
         commit(:get, "payment_intents/#{intent_id}", nil, options)
       end
@@ -56,15 +58,9 @@ module ActiveMerchant #:nodoc:
         post[:card][:exp_month] = payment_method.month
         post[:card][:exp_year] = payment_method.year
         post[:card][:cvc] = payment_method.verification_value if payment_method.verification_value
+        add_billing_address(post, options)
 
         commit(:post, 'payment_methods', post, options)
-      end
-
-      def capture(money, intent_id, options = {})
-        post = {}
-        post[:amount_to_capture] = money
-        add_connected_account(post, options)
-        commit(:post, "payment_intents/#{intent_id}/capture", post, options)
       end
 
       def update_intent(money, intent_id, payment_method, options = {})
@@ -85,6 +81,21 @@ module ActiveMerchant #:nodoc:
         commit(:post, "payment_intents/#{intent_id}", post, options)
       end
 
+      def authorize(money, payment_method, options = {})
+        create_intent(money, payment_method, options.merge!(confirm: true, capture_method: 'manual'))
+      end
+
+      def purchase(money, payment_method, options = {})
+        create_intent(money, payment_method, options.merge!(confirm: true, capture_method: 'automatic'))
+      end
+
+      def capture(money, intent_id, options = {})
+        post = {}
+        post[:amount_to_capture] = money
+        add_connected_account(post, options)
+        commit(:post, "payment_intents/#{intent_id}/capture", post, options)
+      end
+
       def void(intent_id, options = {})
         post = {}
         post[:cancellation_reason] = options[:cancellation_reason] if ALLOWED_CANCELLATION_REASONS.include?(options[:cancellation_reason])
@@ -95,6 +106,41 @@ module ActiveMerchant #:nodoc:
         intent = commit(:get, "payment_intents/#{intent_id}", nil, options)
         charge_id = intent.params.dig('charges', 'data')[0].dig('id')
         super(money, charge_id, options)
+      end
+
+      # Note: Not all payment methods are currently supported by the {Payment Methods API}[https://stripe.com/docs/payments/payment-methods]
+      # Current implementation will create a PaymentMethod object if the method is a token or credit card
+      # All other types will default to legacy Stripe store
+      def store(payment_method, options = {})
+        params = {}
+        post = {}
+
+        # If customer option is provided, create a payment method and attach to customer id
+        # Otherwise, create a customer, then attach
+        if payment_method.is_a?(StripePaymentToken) || payment_method.is_a?(ActiveMerchant::Billing::CreditCard)
+          add_payment_method_token(params, payment_method, options)
+          if options[:customer]
+            customer_id = options[:customer]
+          else
+            post[:validate] = options[:validate] unless options[:validate].nil?
+            post[:description] = options[:description] if options[:description]
+            post[:email] = options[:email] if options[:email]
+            customer = commit(:post, 'customers', post, options)
+            customer_id = customer.params['id']
+          end
+          commit(:post, "payment_methods/#{params[:payment_method]}/attach", { customer: customer_id }, options)
+        else
+          super(payment_method, options)
+        end
+      end
+
+      def unstore(identification, options = {}, deprecated_options = {})
+        if identification.include?('pm_')
+          _, payment_method = identification.split('|')
+          commit(:post, "payment_methods/#{payment_method}/detach", nil, options)
+        else
+          super(identification, options, deprecated_options)
+        end
       end
 
       private
@@ -140,7 +186,14 @@ module ActiveMerchant #:nodoc:
         if payment_method.is_a?(StripePaymentToken)
           post[:payment_method] = payment_method.payment_data['id']
         elsif payment_method.is_a?(String)
-          post[:payment_method] = payment_method
+          if payment_method.include?('|')
+            customer_id, payment_method_id = payment_method.split('|')
+            token = payment_method_id
+            post[:customer] = customer_id
+          else
+            token = payment_method
+          end
+          post[:payment_method] = token
         end
       end
 
@@ -152,18 +205,43 @@ module ActiveMerchant #:nodoc:
         post
       end
 
+      def add_exemption(post, options = {})
+        return unless options[:confirm]
+        post[:payment_method_options] ||= {}
+        post[:payment_method_options][:card] ||= {}
+        post[:payment_method_options][:card][:moto] = true if options[:moto]
+      end
+
       def setup_future_usage(post, options = {})
         post[:setup_future_usage] = options[:setup_future_usage] if %w( on_session off_session ).include?(options[:setup_future_usage])
+        post[:off_session] = options[:off_session] if options[:off_session] && options[:confirm] == true
+        post
       end
 
       def add_connected_account(post, options = {})
-        return unless transfer_data = options[:transfer_data]
+        return unless options[:transfer_destination]
         post[:transfer_data] = {}
-        post[:transfer_data][:destination] = transfer_data[:destination] if transfer_data[:destination]
-        post[:transfer_data][:amount] = transfer_data[:amount] if transfer_data[:amount]
+        post[:transfer_data][:destination] = options[:transfer_destination]
+        post[:transfer_data][:amount] = options[:transfer_amount] if options[:transfer_amount]
         post[:on_behalf_of] = options[:on_behalf_of] if options[:on_behalf_of]
         post[:transfer_group] = options[:transfer_group] if options[:transfer_group]
         post[:application_fee_amount] = options[:application_fee] if options[:application_fee]
+        post
+      end
+
+      def add_billing_address(post, options = {})
+        return unless billing = options[:billing_address] || options[:address]
+        post[:billing_details] = {}
+        post[:billing_details][:address] = {}
+        post[:billing_details][:address][:city] = billing[:city] if billing[:city]
+        post[:billing_details][:address][:country] = billing[:country] if billing[:country]
+        post[:billing_details][:address][:line1] = billing[:address1] if billing[:address1]
+        post[:billing_details][:address][:line2] = billing[:address2] if billing[:address2]
+        post[:billing_details][:address][:postal_code] = billing[:zip] if billing[:zip]
+        post[:billing_details][:address][:state] = billing[:state] if billing[:state]
+        post[:billing_details][:email] = billing[:email] if billing[:email]
+        post[:billing_details][:name] = billing[:name] if billing[:name]
+        post[:billing_details][:phone] = billing[:phone] if billing[:phone]
         post
       end
 
